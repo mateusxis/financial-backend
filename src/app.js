4c5bd7dc672ff37c61e69b0e1f2b56dda12c09be
mateusxis <mateusx@poli.ufrj.br>
2021-01-18 11:40:16 -0300

    refactor(server): ✨ add config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const compress = require('koa-compress');
+const helmet = require('koa-helmet');
 
 const router = require('./router');
 
 const app = new Koa();
 
-app.use(router.routes());
+app
+  .use(helmet())
+  .use(compress())
+  .use(bodyParser({ enableTypes: ['json'] }))
+  .use(router.routes());
 
 module.exports = app;
