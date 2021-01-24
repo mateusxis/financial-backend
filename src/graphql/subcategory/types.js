6b9f573c91c54730fffdc5ee9bcfcb853cc07ab7
Mateus Barbosa <mateusx@poli.ufrj.br>
2021-01-24 12:35:59 -0300

    feat(subcategory): ✨ add types

---
+++ src/graphql/subcategory/types.js
@@ -0,0 +1,44 @@
+const {
+  extendType, nonNull, objectType, stringArg,
+} = require('nexus');
+
+const Subcategory = objectType({
+  name: 'Subcategory',
+  definition(t) {
+    t.model.id();
+    t.model.subcategory();
+    t.model.createdAt();
+  },
+});
+
+const addSubcategoryQueries = extendType({
+  type: 'Query',
+  definition(t) {
+    t.crud.subcategory();
+    t.crud.subcategories();
+  },
+});
+
+const addSubcategoryMutations = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.field('createSubcategory', {
+      type: 'Subcategory',
+      args: {
+        subcategory: nonNull(stringArg()),
+      },
+      resolve: async (_, { subcategory }, ctx) =>
+        ctx.prisma.subcategory.create({
+          data: {
+            subcategory,
+          },
+        }),
+    });
+  },
+});
+
+module.exports = {
+  Subcategory,
+  addSubcategoryQueries,
+  addSubcategoryMutations,
+};
