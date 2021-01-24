6ca57a22ca52410e495be4ab41013077b6c67219
Mateus Barbosa <mateusx@poli.ufrj.br>
2021-01-24 12:34:18 -0300

    feat(category): ✨ add types

---
+++ src/graphql/category/types.js
@@ -0,0 +1,40 @@
+const {
+  extendType, nonNull, objectType, stringArg,
+} = require('nexus');
+
+const Category = objectType({
+  name: 'Category',
+  definition(t) {
+    t.model.id();
+    t.model.category();
+    t.model.createdAt();
+  },
+});
+
+const addCategoryQueries = extendType({
+  type: 'Query',
+  definition(t) {
+    t.crud.category();
+    t.crud.categories();
+  },
+});
+
+const addCategoryMutations = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.field('createCategory', {
+      type: 'Category',
+      args: {
+        category: nonNull(stringArg()),
+      },
+      resolve: async (_, { category }, ctx) =>
+        ctx.prisma.category.create({
+          data: {
+            category,
+          },
+        }),
+    });
+  },
+});
+
+module.exports = { Category, addCategoryQueries, addCategoryMutations };
