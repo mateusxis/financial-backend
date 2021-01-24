48ab34ea63f9849d3a8cc5a5676ed93599b4e7e9
Mateus Barbosa <mateusx@poli.ufrj.br>
2021-01-24 12:35:23 -0300

    feat(origin): ✨ add types

---
+++ src/graphql/origin/types.js
@@ -0,0 +1,44 @@
+const {
+  extendType, nonNull, objectType, stringArg,
+} = require('nexus');
+
+const Origin = objectType({
+  name: 'Origin',
+  definition(t) {
+    t.model.id();
+    t.model.origin();
+    t.model.createdAt();
+  },
+});
+
+const addOriginQueries = extendType({
+  type: 'Query',
+  definition(t) {
+    t.crud.origin();
+    t.crud.origins();
+  },
+});
+
+const addOriginMutations = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.field('createOrigin', {
+      type: 'Origin',
+      args: {
+        origin: nonNull(stringArg()),
+      },
+      resolve: async (_, { origin }, ctx) =>
+        ctx.prisma.origin.create({
+          data: {
+            origin,
+          },
+        }),
+    });
+  },
+});
+
+module.exports = {
+  Origin,
+  addOriginQueries,
+  addOriginMutations,
+};
