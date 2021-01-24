488b60ebe2030b5a34922b8eead2ba3e43a1c94e
Mateus Barbosa <mateusx@poli.ufrj.br>
2021-01-24 12:36:55 -0300

    refactor(schemas): ♻️ uptdate prisma and graphql

--- src/graphql/generated/nexus.ts
+++ src/graphql/generated/nexus.ts
@@ -19,6 +19,29 @@ declare global {
 }
 
 export interface NexusGenInputs {
+  CategoryWhereUniqueInput: { // input type
+    id?: number | null; // Int
+  }
+  ClassUniCategorySubCompoundUniqueInput: { // input type
+    categoryId: number; // Int!
+    subcategoryId: number; // Int!
+  }
+  ClassWhereUniqueInput: { // input type
+    id?: number | null; // Int
+    uniCategorySub?: NexusGenInputs['ClassUniCategorySubCompoundUniqueInput'] | null; // ClassUniCategorySubCompoundUniqueInput
+  }
+  MethodPaymentWhereUniqueInput: { // input type
+    id?: number | null; // Int
+  }
+  OriginWhereUniqueInput: { // input type
+    id?: number | null; // Int
+  }
+  SectorWhereUniqueInput: { // input type
+    id?: number | null; // Int
+  }
+  SubcategoryWhereUniqueInput: { // input type
+    id?: number | null; // Int
+  }
   UserWhereUniqueInput: { // input type
     email?: string | null; // String
     id?: number | null; // Int
@@ -34,12 +57,45 @@ export interface NexusGenScalars {
   Float: number
   Boolean: boolean
   ID: string
+  DateTime: any
 }
 
 export interface NexusGenObjects {
+  Category: { // root type
+    category: string; // String!
+    createdAt: NexusGenScalars['DateTime']; // DateTime!
+    id: number; // Int!
+  }
+  Class: { // root type
+    categoryId?: number | null; // Int
+    createdAt: NexusGenScalars['DateTime']; // DateTime!
+    id: number; // Int!
+    subcategoryId?: number | null; // Int
+  }
+  MethodPayment: { // root type
+    createdAt: NexusGenScalars['DateTime']; // DateTime!
+    id: number; // Int!
+    methodPayment: string; // String!
+  }
   Mutation: {};
+  Origin: { // root type
+    createdAt: NexusGenScalars['DateTime']; // DateTime!
+    id: number; // Int!
+    origin: string; // String!
+  }
   Query: {};
+  Sector: { // root type
+    createdAt: NexusGenScalars['DateTime']; // DateTime!
+    id: number; // Int!
+    sector: string; // String!
+  }
+  Subcategory: { // root type
+    createdAt: NexusGenScalars['DateTime']; // DateTime!
+    id: number; // Int!
+    subcategory: string; // String!
+  }
   User: { // root type
+    createdAt: NexusGenScalars['DateTime']; // DateTime!
     email: string; // String!
     id: number; // Int!
     name: string; // String!
@@ -58,13 +114,66 @@ export type NexusGenRootTypes = NexusGenObjects
 export type NexusGenAllTypes = NexusGenRootTypes & NexusGenScalars
 
 export interface NexusGenFieldTypes {
+  Category: { // field return type
+    category: string; // String!
+    createdAt: NexusGenScalars['DateTime']; // DateTime!
+    id: number; // Int!
+  }
+  Class: { // field return type
+    category: NexusGenRootTypes['Category'] | null; // Category
+    categoryId: number | null; // Int
+    createdAt: NexusGenScalars['DateTime']; // DateTime!
+    id: number; // Int!
+    subcategory: NexusGenRootTypes['Subcategory'] | null; // Subcategory
+    subcategoryId: number | null; // Int
+  }
+  MethodPayment: { // field return type
+    createdAt: NexusGenScalars['DateTime']; // DateTime!
+    id: number; // Int!
+    methodPayment: string; // String!
+  }
   Mutation: { // field return type
+    createCategory: NexusGenRootTypes['Category'] | null; // Category
+    createClass: NexusGenRootTypes['Class'] | null; // Class
+    createMethodPayment: NexusGenRootTypes['MethodPayment'] | null; // MethodPayment
+    createOrigin: NexusGenRootTypes['Origin'] | null; // Origin
+    createSector: NexusGenRootTypes['Sector'] | null; // Sector
+    createSubcategory: NexusGenRootTypes['Subcategory'] | null; // Subcategory
     createUser: NexusGenRootTypes['User'] | null; // User
   }
+  Origin: { // field return type
+    createdAt: NexusGenScalars['DateTime']; // DateTime!
+    id: number; // Int!
+    origin: string; // String!
+  }
   Query: { // field return type
+    categories: NexusGenRootTypes['Category'][]; // [Category!]!
+    category: NexusGenRootTypes['Category'] | null; // Category
+    class: NexusGenRootTypes['Class'] | null; // Class
+    classes: NexusGenRootTypes['Class'][]; // [Class!]!
+    methodPayment: NexusGenRootTypes['MethodPayment'] | null; // MethodPayment
+    methodPayments: NexusGenRootTypes['MethodPayment'][]; // [MethodPayment!]!
+    origin: NexusGenRootTypes['Origin'] | null; // Origin
+    origins: NexusGenRootTypes['Origin'][]; // [Origin!]!
+    sector: NexusGenRootTypes['Sector'] | null; // Sector
+    sectors: NexusGenRootTypes['Sector'][]; // [Sector!]!
+    subcategories: NexusGenRootTypes['Subcategory'][]; // [Subcategory!]!
+    subcategory: NexusGenRootTypes['Subcategory'] | null; // Subcategory
     user: NexusGenRootTypes['User'] | null; // User
+    users: NexusGenRootTypes['User'][]; // [User!]!
+  }
+  Sector: { // field return type
+    createdAt: NexusGenScalars['DateTime']; // DateTime!
+    id: number; // Int!
+    sector: string; // String!
+  }
+  Subcategory: { // field return type
+    createdAt: NexusGenScalars['DateTime']; // DateTime!
+    id: number; // Int!
+    subcategory: string; // String!
   }
   User: { // field return type
+    createdAt: NexusGenScalars['DateTime']; // DateTime!
     email: string; // String!
     id: number; // Int!
     name: string; // String!
@@ -73,13 +182,66 @@ export interface NexusGenFieldTypes {
 }
 
 export interface NexusGenFieldTypeNames {
+  Category: { // field return type name
+    category: 'String'
+    createdAt: 'DateTime'
+    id: 'Int'
+  }
+  Class: { // field return type name
+    category: 'Category'
+    categoryId: 'Int'
+    createdAt: 'DateTime'
+    id: 'Int'
+    subcategory: 'Subcategory'
+    subcategoryId: 'Int'
+  }
+  MethodPayment: { // field return type name
+    createdAt: 'DateTime'
+    id: 'Int'
+    methodPayment: 'String'
+  }
   Mutation: { // field return type name
+    createCategory: 'Category'
+    createClass: 'Class'
+    createMethodPayment: 'MethodPayment'
+    createOrigin: 'Origin'
+    createSector: 'Sector'
+    createSubcategory: 'Subcategory'
     createUser: 'User'
   }
+  Origin: { // field return type name
+    createdAt: 'DateTime'
+    id: 'Int'
+    origin: 'String'
+  }
   Query: { // field return type name
+    categories: 'Category'
+    category: 'Category'
+    class: 'Class'
+    classes: 'Class'
+    methodPayment: 'MethodPayment'
+    methodPayments: 'MethodPayment'
+    origin: 'Origin'
+    origins: 'Origin'
+    sector: 'Sector'
+    sectors: 'Sector'
+    subcategories: 'Subcategory'
+    subcategory: 'Subcategory'
     user: 'User'
+    users: 'User'
+  }
+  Sector: { // field return type name
+    createdAt: 'DateTime'
+    id: 'Int'
+    sector: 'String'
+  }
+  Subcategory: { // field return type name
+    createdAt: 'DateTime'
+    id: 'Int'
+    subcategory: 'String'
   }
   User: { // field return type name
+    createdAt: 'DateTime'
     email: 'String'
     id: 'Int'
     name: 'String'
@@ -89,15 +251,94 @@ export interface NexusGenFieldTypeNames {
 
 export interface NexusGenArgTypes {
   Mutation: {
+    createCategory: { // args
+      category: string; // String!
+    }
+    createClass: { // args
+      categoryId: number; // Int!
+      subcategoryId: number; // Int!
+    }
+    createMethodPayment: { // args
+      methodPayment: string; // String!
+    }
+    createOrigin: { // args
+      origin: string; // String!
+    }
+    createSector: { // args
+      sector: string; // String!
+    }
+    createSubcategory: { // args
+      subcategory: string; // String!
+    }
     createUser: { // args
-      email?: string | null; // String
+      email: string; // String!
       name: string; // String!
       password: string; // String!
     }
   }
   Query: {
+    categories: { // args
+      after?: NexusGenInputs['CategoryWhereUniqueInput'] | null; // CategoryWhereUniqueInput
+      before?: NexusGenInputs['CategoryWhereUniqueInput'] | null; // CategoryWhereUniqueInput
+      first?: number | null; // Int
+      last?: number | null; // Int
+    }
+    category: { // args
+      where: NexusGenInputs['CategoryWhereUniqueInput']; // CategoryWhereUniqueInput!
+    }
+    class: { // args
+      where: NexusGenInputs['ClassWhereUniqueInput']; // ClassWhereUniqueInput!
+    }
+    classes: { // args
+      after?: NexusGenInputs['ClassWhereUniqueInput'] | null; // ClassWhereUniqueInput
+      before?: NexusGenInputs['ClassWhereUniqueInput'] | null; // ClassWhereUniqueInput
+      first?: number | null; // Int
+      last?: number | null; // Int
+    }
+    methodPayment: { // args
+      where: NexusGenInputs['MethodPaymentWhereUniqueInput']; // MethodPaymentWhereUniqueInput!
+    }
+    methodPayments: { // args
+      after?: NexusGenInputs['MethodPaymentWhereUniqueInput'] | null; // MethodPaymentWhereUniqueInput
+      before?: NexusGenInputs['MethodPaymentWhereUniqueInput'] | null; // MethodPaymentWhereUniqueInput
+      first?: number | null; // Int
+      last?: number | null; // Int
+    }
+    origin: { // args
+      where: NexusGenInputs['OriginWhereUniqueInput']; // OriginWhereUniqueInput!
+    }
+    origins: { // args
+      after?: NexusGenInputs['OriginWhereUniqueInput'] | null; // OriginWhereUniqueInput
+      before?: NexusGenInputs['OriginWhereUniqueInput'] | null; // OriginWhereUniqueInput
+      first?: number | null; // Int
+      last?: number | null; // Int
+    }
+    sector: { // args
+      where: NexusGenInputs['SectorWhereUniqueInput']; // SectorWhereUniqueInput!
+    }
+    sectors: { // args
+      after?: NexusGenInputs['SectorWhereUniqueInput'] | null; // SectorWhereUniqueInput
+      before?: NexusGenInputs['SectorWhereUniqueInput'] | null; // SectorWhereUniqueInput
+      first?: number | null; // Int
+      last?: number | null; // Int
+    }
+    subcategories: { // args
+      after?: NexusGenInputs['SubcategoryWhereUniqueInput'] | null; // SubcategoryWhereUniqueInput
+      before?: NexusGenInputs['SubcategoryWhereUniqueInput'] | null; // SubcategoryWhereUniqueInput
+      first?: number | null; // Int
+      last?: number | null; // Int
+    }
+    subcategory: { // args
+      where: NexusGenInputs['SubcategoryWhereUniqueInput']; // SubcategoryWhereUniqueInput!
+    }
     user: { // args
       where: NexusGenInputs['UserWhereUniqueInput']; // UserWhereUniqueInput!
+    }
+    users: { // args
+      after?: NexusGenInputs['UserWhereUniqueInput'] | null; // UserWhereUniqueInput
+      before?: NexusGenInputs['UserWhereUniqueInput'] | null; // UserWhereUniqueInput
+      first?: number | null; // Int
+      last?: number | null; // Int
     }
   }
 }
--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -2,10 +2,24 @@ const path = require('path');
 const { makeSchema } = require('nexus');
 const { nexusPrisma } = require('nexus-plugin-prisma');
 
+const { addCategoryMutations, addCategoryQueries, Category } = require('./category/types');
+const { addClassMutations, addClassQueries, Class } = require('./class/types');
+const { addMethodPaymentMutations, addMethodPaymentQueries, MethodPayment } = require('./methodPayment/types');
+const { addOriginMutations, addOriginQueries, Origin } = require('./origin/types');
+const { addSectorMutations, addSectorQueries, Sector } = require('./sector/types');
+const { addSubcategoryMutations, addSubcategoryQueries, Subcategory } = require('./subcategory/types');
 const { addUserMutations, addUserQueries, User } = require('./user/types');
 
 const schema = makeSchema({
-  types: [addUserMutations, addUserQueries, User],
+  types: [
+    addCategoryMutations, addCategoryQueries, Category,
+    addClassMutations, addClassQueries, Class,
+    addMethodPaymentMutations, addMethodPaymentQueries, MethodPayment,
+    addOriginMutations, addOriginQueries, Origin,
+    addSectorMutations, addSectorQueries, Sector,
+    addSubcategoryMutations, addSubcategoryQueries, Subcategory,
+    addUserMutations, addUserQueries, User,
+  ],
   plugins: [nexusPrisma({ experimentalCRUD: true })],
   outputs: {
     schema: path.join(__dirname, '/../schema.graphql'),
