f9c6a71694b0eb9900ab4d752abdf56b6f9daaeb
Mateus Barbosa <mateusx@poli.ufrj.br>
2021-03-01 09:54:16 -0300

    feat(app): add database in server

--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const compress = require('koa-compress');
 const helmet = require('koa-helmet');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const router = require('./router');
 const { apolloServer } = require('./apollo');
+const database = require('./config/database');
 
 const app = new Koa();
+
+database();
 
 apolloServer.applyMiddleware({ app });
 
